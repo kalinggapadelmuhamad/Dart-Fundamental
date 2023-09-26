e7cd8c5c5e47a87d045cd6d7d0f409ae1dcaac39
kalinggapadelmuhamad <enginerbros@gmail.com>
2023-09-26 17:33:52 +0700

    Update : Update add new fundamental dart programs

---
+++ programmerLogic/programmer_logic/bin/programmer_logic.dart
@@ -0,0 +1,12 @@
+import 'package:programmer_logic/programmer_logic.dart' as programmer_logic;
+
+void main(List<String> arguments) {
+  int oilThatShouldBuy = 1;
+  bool isEgg = false;
+
+  if (isEgg) {
+    oilThatShouldBuy = 6;
+  }
+
+  print(oilThatShouldBuy);
+}
